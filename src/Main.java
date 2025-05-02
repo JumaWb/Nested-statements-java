3e839544def6f7ab9ce1d018d7784f31df84e0a5
Brian Juma <bjuma0985@gmail.com>
2025-05-02 13:47:41 +0300

    nests in java

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         boolean isStudent = false;
-        boolean isSenior;
+        boolean isSenior = true;
         double price = 9.99;
 
         if (isStudent) {
+            if (isSenior){
+                System.out.println("You get a senior discount of 20%");
+                System.out.println("You get a student discount of 10%");
+                price *= 0.7;
+            }
             System.out.println("You get a student discount of 10%");
             price *= 0.9;
         } else {
-            price *= 1;
+            if (isSenior){
+                System.out.println("You get a senior discount of 20%");
+                price *=0.8;
+            }
         }
         System.out.println("The price of a ticket is: kes" + price );
     }
